f4cd0247d46e8b0755b5741ebd38d45663db5530
ahmadiftme8 <ahmadiftme99@gmail.com>
2024-06-25 19:17:13 +0330

    adding arrow down

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,26 @@
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
+/* // script.js
+const fadeDiv = document.querySelector('.down-arrow-container');
+let cycles = 0;
+const totalCycles = 5;
 
-TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
 
-    if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
+function fadeInOut() {
+    
+} */
 
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+/* function fadeInOut() {
+  fadeDiv.style.animation = 'fadeInOut 2s ease-in-out';
+  
+  fadeDiv.addEventListener('animationend', handleAnimationEnd, { once: true });
+}
 
-    var that = this;
-    var delta = 200 - Math.random() * 100;
+function handleAnimationEnd() {
+  cycles++;
+  if (cycles < totalCycles) {
+    fadeInOut();
+  } else {
+    fadeDiv.style.opacity = 0;
+  }
+}
 
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-    }
-
-    setTimeout(function() {
-    that.tick();
-    }, delta);
-};
-
-window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
-};
+fadeInOut(); */
